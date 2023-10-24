2ff7c800a421015626f7e283e7cb6a7507158c28
glenloshie5647 <glenloshie5647@gmail.com>
2023-10-24 20:29:43 +0000

    Initial commit

---
+++ sophisticated_code.js
@@ -0,0 +1,155 @@
+/* 
+Filename: sophisticated_code.js
+
+This code is an advanced data structure called a Binary Search Tree (BST). It implements insertion, deletion, searching, and traversal functionalities. 
+The BST class consists of a Node class that represents each node in the tree, and various methods to manipulate the tree structure.
+
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, key) {
+    if (node === null) {
+      return null;
+    }
+
+    if (key < node.value) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    } else if (key > node.value) {
+      node.right = this.removeNode(node.right, key);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      const minValue = this.findMinNode(node.right);
+      node.value = minValue.value;
+      node.right = this.removeNode(node.right, minValue.value);
+      return node;
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    } else {
+      return true;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    }
+
+    return this.findMinNode(node.left);
+  }
+
+  inOrderTraversal(callback) {
+    this.inOrderTraversalNode(this.root, callback);
+  }
+
+  inOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inOrderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inOrderTraversalNode(node.right, callback);
+    }
+  }
+  // ... other traversal methods like preOrder, postOrder, etc.
+}
+
+// Example usage of the BST class
+
+const bst = new BST();
+
+// Insert nodes
+bst.insert(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(20);
+bst.insert(40);
+bst.insert(60);
+bst.insert(80);
+
+// Remove a node
+bst.remove(20);
+
+// Search for a node
+console.log(bst.search(40)); // Output: true
+
+// Traverse the tree using in-order traversal
+bst.inOrderTraversal(console.log);
+/* Output:
+20
+30
+40
+50
+60
+70
+80
+*/
+
+// ... other operations and methods can be performed on the BST class
